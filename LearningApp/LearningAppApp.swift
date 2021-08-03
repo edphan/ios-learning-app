c2cbc894ae31884c9481da00b1117230147521bd
Ed <76992905+edphan@users.noreply.github.com>
2021-08-03 15:09:02 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Edward Phan on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
